6d7180ab5e5cd26090aad5c49b0f2872a4c301e1
Palagin Igor <igor_palagin.56rus@mail.ru>
2023-05-21 12:57:21 +0300

    Добавьте файлы проекта.

---
+++ 8.6.2/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+string folderPath = @"C:\Users\igor_\OneDrive\Рабочий стол\SizeFolder";
+
+try
+{
+    long folderSize = FolderSize(folderPath);
+    Console.WriteLine($"Размер папки {folderPath}: {folderSize} байт");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ошибка: {ex.Message}");
+}
+
+static long FolderSize(string folderPath)
+{
+    long folderSize = 0;
+    DirectoryInfo directoryInfo = new DirectoryInfo(folderPath);
+    if (!directoryInfo.Exists)
+    {
+        throw new DirectoryNotFoundException($"Папка {folderPath} не найдена");
+    }
+    FileInfo[] files = directoryInfo.GetFiles();
+    foreach (FileInfo file in files)
+    {
+        folderSize += file.Length;
+    }
+   
+    DirectoryInfo[] subDirs = directoryInfo.GetDirectories();
+    foreach(DirectoryInfo subDir in subDirs)
+    {
+        folderSize += FolderSize(subDir.FullName);
+    }
+    return folderSize;
+}
+
